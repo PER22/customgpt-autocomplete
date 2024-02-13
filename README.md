c62c099460fc33cdefe786c316eb5568ea00e374
Padraic Reilly <patrick.reilly22@gmail.com>
2024-02-13 14:53:23 -0800

    assignment complete

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# customgpt-autocomplete
+## Autocomplete in python from a text file:
+
+#### The goal was to mimic a google search, in that a partial query being typed prompted the system to return autocomplete suggestions.
+
+There is no elastic aspect or scoring aspect, so typoes are not resolved in the code at all.
+For example, searching `howt o train your d` will not return `[how to train your dog', 'how to train your delts', 'how to train your dragon']`, but might instead return `[]` if `howt` does not appear in any queries.
+
+Aside from that aspect, I am happy with this code.
+
+At first I built a standard trie, but the resulting response time was poor, as the suggestion process included traversing through every query that shared the current prefix. As building the trie is a one-time process here, I decided to front-load the process of collecting the matching queries into that step as well. 
+
+I modified the standard trie by giving each node a sorted array of size k (default is 4, as mentioned in the assignment sheet), and populating these arrays recusively with the highest frequency query strings in any child nodes. As a result, the suggestion part of the logic is very fast, at the cost of a more memory-intensive trie. 
+
+I will have to learn how to time functions in python, and then I will add that to the code, and put some examples of queries, responses, and times, as well as measuring the building time for the trie.
+
+I had some difficulties along the way: I failed to notice that although the queries have associated frequencies, they are not unique. As a result, I was getting responses that didn't match the data. However, upon finding this, it all clicked together nicely.
--- autocomplete.py
+++ autocomplete.py
@@ -1,16 +1,15 @@
+import sys
 from flask import Flask, request
 
 app = Flask(__name__)
-import sys
+
 sys.setrecursionlimit(1500)
 
-
-#Will store the most common results for the trie node that contains it
 class SortedFixedSizeArray:
     def __init__(self, max_size=4):
         self.arr = []
         self.max_size = max_size
-    def insert(self, tuple):    
+    def __insert__(self, tuple):
         self.arr.append(tuple)
         self.arr.sort(key=lambda x: -x[1])
         self.arr = self.arr[:self.max_size]
@@ -34,30 +33,20 @@ class Trie:
                 node.children[char] = TrieNode()
             node = node.children[char]
         node.is_end_of_word = True
-        node.frequency = frequency
+        node.frequency += frequency
     
     def prioritize(self):
         self._prioritize(self.root, '')
     
-    def _prioritize(self, node, prefix): 
-        for char, child in node.children.items():
-            self._prioritize(child, prefix + char) 
-
-        
+    def _prioritize(self, node, prefix):
         if node.is_end_of_word:
-            node.most_frequent_suffixes.insert((prefix, node.frequency))
-
-        aggregated_suffixes = {}
-        for child in node.children.values():
-            for suffix, freq in child.most_frequent_suffixes.arr:
-                if suffix not in aggregated_suffixes or aggregated_suffixes[suffix] < freq:
-                    aggregated_suffixes[suffix] = freq
-
-        # Insert aggregated suffixes ensuring the top N are kept
-        for suffix, freq in aggregated_suffixes.items():
-            node.most_frequent_suffixes.insert((suffix, freq))
-
-
+            node.most_frequent_suffixes.__insert__((prefix, node.frequency))
+        
+        for char, child in node.children.items():
+            self._prioritize(child, prefix + char)
+            for suffix_tuple in child.most_frequent_suffixes.arr:
+                node.most_frequent_suffixes.__insert__(suffix_tuple)
+        
     def suggest(self, prefix):
         node = self.root
         for char in prefix:
@@ -74,7 +63,6 @@ with open('medical-questions', 'r') as file:
   for line in file:
     query, frequency = (lambda parts: (parts[0], int(parts[1])))(line.strip().split('\t'))
     trie.insert(query=query, frequency=frequency)
-
     
 print("Data loaded successfully, now prioritizing")
 
@@ -82,13 +70,10 @@ trie.prioritize()
 
 print('Trie is prioritized, ready for queries')
 
-
-
-
 @app.route('/s')
 def autocomplete():
     query = request.args.get('q', '')
     return trie.suggest(query)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
